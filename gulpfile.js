b37f402c26dab5d1cd2f2d98a98c614bb6394695
James Irwin <rebolyte@gmail.com>
2015-02-17 17:47:17 -0700

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp	= require('gulp');
 var deploy	= require('gulp-gh-pages');
 
+
+gulp.task('build', function () {
+  gulp.src('./src/**/*')
+    .pipe(gulp.dest('./dist'));
+})
+
 /**
  * Push build to gh-pages
  */
