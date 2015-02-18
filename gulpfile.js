d3cac02b29b29ee0ed677a0158507b0d525c1933
James Irwin <rebolyte@gmail.com>
2015-02-17 17:49:41 -0700

    Add references to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp	= require('gulp');
 var deploy	= require('gulp-gh-pages');
 
+// http://charliegleason.com/articles/deploying-to-github-pages-with-gulp
+// http://www.michael-whelan.net/deploying-sandra-snow-to-github-pages-with-gulp/
+// https://github.com/rowoot/gulp-gh-pages
 
 gulp.task('build', function () {
   gulp.src('./src/**/*')
